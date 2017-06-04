40b590e021dfc3b68757c185d040b8b4da984f16
Kyle <kiloloco@me.com>
2017-06-04 14:52:25 -0700

    finished tutorial instyle

---
+++ Geofencing Region/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Geofencing Region
+//
+//  Created by Kyle Lee on 6/4/17.
+//  Copyright © 2017 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import UserNotifications
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in }
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+
+    
+    @IBAction func addRegion(_ sender: Any) {
+        guard let longPress = sender as? UILongPressGestureRecognizer else { return }
+        let touchLocation = longPress.location(in: mapView)
+        let coordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
+        let region = CLCircularRegion(center: coordinate, radius: 200, identifier: "geofence")
+        mapView.removeOverlays(mapView.overlays)
+        locationManager.startMonitoring(for: region)
+        let circle = MKCircle(center: coordinate, radius: region.radius)
+        mapView.add(circle)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showNotification(title: String, message: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.badge = 1
+        content.sound = .default()
+        let request = UNNotificationRequest(identifier: "notif", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        mapView.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        let title = "You Entered the Region"
+        let message = "Wow theres cool stuff in here! YAY!"
+        showAlert(title: title, message: message)
+        showNotification(title: title, message: message)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        let title = "You Left the Region"
+        let message = "Say bye bye to all that cool stuff. =["
+        showAlert(title: title, message: message)
+        showNotification(title: title, message: message)
+    }
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let circleOverlay = overlay as? MKCircle else { return MKOverlayRenderer() }
+        let circleRenderer = MKCircleRenderer(circle: circleOverlay)
+        circleRenderer.strokeColor = .red
+        circleRenderer.fillColor = .red
+        circleRenderer.alpha = 0.5
+        return circleRenderer
+    }
+}
+
+
+
+
+
+
+
+
